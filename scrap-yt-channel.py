749e5e7361d181a98f9f5f11fbe34e46ce2b4e28
spiel_meister <81038761+spiel0meister@users.noreply.github.com>
2023-08-21 03:26:40 +0200

    added script

---
+++ scrap-yt-channel.py
@@ -0,0 +1,35 @@
+from requests_html import HTMLSession
+from sys import argv
+import pandas as pd
+
+URL = "https://www.youtube.com"
+session = HTMLSession()
+channel_id = ""
+try:
+    channel_id = argv[1]
+except IndexError:
+    channel_id = input("Enter Channel ID (it looks like @some_id): ")
+
+def make_link(href: str) -> str:
+    return f"{URL}/{href}"
+
+site = session.get(f"{URL}/{channel_id}/videos")
+site.html.render(sleep=1, keep_page=True, scrolldown=250)
+
+video_objects = []
+
+videos = site.html.xpath('//*[@id="video-title-link"]')
+for video in videos:
+    href = video.attrs["href"][1:]
+    title = video.find("yt-formatted-string")[0].text 
+
+    video = {
+        "title": title,
+        "link": make_link(href)
+    }
+
+    print(video)
+    video_objects.append(video)
+
+videos_df = pd.DataFrame(video_objects)
+videos_df.to_csv(f"{channel_id.replace('@', '')}.csv", index=False)
